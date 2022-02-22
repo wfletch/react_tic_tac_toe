f23278e568466201a9f0818ead70666f3582c418
Warren Fletcher <warren@fletch.band>
2022-02-23 00:32:13 +0100

    Added Ability Store State

--- ttt/src/index.js
+++ ttt/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     value : null,
+   }
+  }
   render() {
     return (
-      <button className="square" onClick={(e) => console.log(e)}>
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
       {/* // <button className="square" onClick={function(){console.log(this.value)}}> */}
-        {this.props.value}
+        {this.state.value}
       </button>
     );
   }
